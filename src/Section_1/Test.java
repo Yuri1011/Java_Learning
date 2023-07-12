c9eb40d14b97d997bec684e6e2541d6f96b80a06
Yury Trypolski <yury1011.89@gmail.com>
2023-07-12 14:40:41 +0300

    add Test

---
+++ src/Section_1/Test.java
@@ -0,0 +1,15 @@
+package Section_1;
+
+public class Test {
+    public static void main(String[] args) {
+        /*
+        var a = 1;
+        var b = ++a;
+        var c = a++;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        */
+    }
+}
