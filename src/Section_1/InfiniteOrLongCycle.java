14bff7a464ceb32f97fb01b601ffac2e49dc3c48
Yury Trypolski <yury1011.89@gmail.com>
2023-07-21 16:35:48 +0300

    added check long cycle

---
+++ src/Section_1/InfiniteOrLongCycle.java
@@ -0,0 +1,14 @@
+package Section_1;
+
+public class InfiniteOrLongCycle {
+    public static void main(String[] args) {
+        long time = System.nanoTime();
+
+        for (int i = 0; i <= 10_000_000; i++) {
+            System.currentTimeMillis();
+        }
+
+        long result = System.nanoTime() - time;
+        System.out.println((double) result / 1_000_000_000 + " seconds");
+    }
+}
