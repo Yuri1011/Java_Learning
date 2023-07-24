c8ca30616a636d81c7ecd24139bfa13587e530dd
Yury Trypolski <yury1011.89@gmail.com>
2023-07-24 11:27:15 +0300

    add string check method

--- src/Section_1/StringMethods.java
+++ src/Section_1/StringMethods.java
@@ -4,10 +4,15 @@ import java.util.Arrays;
 
 public class StringMethods {
     public static void main(String[] args) {
-        String str = "Hello";
+        String str = "Hello World!";
 
         System.out.println(str.length());// show string length
         System.out.println(str.charAt(0));//get character from array by index
         System.out.println(Arrays.toString(str.toCharArray()));// make chars array from string
+
+        System.out.println(str.contains("World"));// check contain word in string
+        System.out.println(str.isEmpty());// check string for empty
+        System.out.println(str.indexOf('l'));// show the first match character in string
+        System.out.println(str.lastIndexOf('l'));// show the last match character in string
     }
 }
