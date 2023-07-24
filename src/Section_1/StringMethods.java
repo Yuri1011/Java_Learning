b96a165d603ec3ecfa933fe30de0bb284ec1d70f
Yury Trypolski <yury1011.89@gmail.com>
2023-07-24 11:56:21 +0300

    add string change methods

--- src/Section_1/StringMethods.java
+++ src/Section_1/StringMethods.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class StringMethods {
     public static void main(String[] args) {
-        String str = "Hello World!";
+        String str = " Hello,World,! ";
 
         System.out.println(str.length());// show string length
         System.out.println(str.charAt(0));//get character from array by index
@@ -14,5 +14,13 @@ public class StringMethods {
         System.out.println(str.isEmpty());// check string for empty
         System.out.println(str.indexOf('l'));// show the first match character in string
         System.out.println(str.lastIndexOf('l'));// show the last match character in string
+
+        System.out.println(str.repeat(3));// repeats a string a given number of times
+        System.out.println(str.replace('H','J'));//change characters in string
+        System.out.println(str.replace("Hello","Bye"));//change worlds in string
+        System.out.println(str.substring(5));//get substring by index
+        System.out.println(Arrays.toString(str.split(",")));// make array from string by any symbol
+//        System.out.println(str.trim());
+        System.out.println(str.strip());// remove gaps around string same as trim()
     }
 }
