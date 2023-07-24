432963451b47afa93324e0356b687d739423de5f
Yury Trypolski <yury1011.89@gmail.com>
2023-07-24 20:39:59 +0300

    add converter types

---
+++ src/Section_1/ConverterTypes.java
@@ -0,0 +1,14 @@
+package Section_1;
+
+public class ConverterTypes {
+    String intFromStr = String.valueOf(1);
+    String doubleFromStr = String.valueOf(1.1);
+    String bollFromStr = String.valueOf(true);
+    String charFromStr = String.valueOf('c');
+
+    int strFromInt = Integer.parseInt("12");
+    double strFromDouble = Double.parseDouble("1.2");
+    boolean strFromBool = Boolean.parseBoolean("true");
+    char charFromStr2 = "a".charAt(0);
+
+}
--- src/Section_1/Test.java
+++ src/Section_1/Test.java
@@ -29,11 +29,16 @@ public class Test {
 //            builder.append(arrChars[i]);
 //        }
 //second way
-        for (int i = str.length() - 1; i >= 0; i--) {
-            builder.append(str.charAt(i));
-        }
+//        for (int i = str.length() - 1; i >= 0; i--) {
+//            builder.append(str.charAt(i));
+//        }
+//third way
+//        for (int i = 0; i < str.length(); i++) {
+//            builder.insert(0,str.charAt(i));
+//        }
+//fourth way
+        builder.append(str).reverse();
         String newStr = builder.toString();
         System.out.println("===>>>   " + newStr);
-
     }
 }
